8b9c58bedf6dffa2d27f0532c6964450bce52a9c
RafaLopezZz <142743097+RafaLopezZz@users.noreply.github.com>
2024-09-13 17:15:48 +0200

    Add files via upload

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/RDP_00.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rdp_00;
+
+/**
+ *
+ * @author rlpmu
+ */
+public class RDP_00 {
+        //https://dev.java/
+        //Comentario simple
+        /*
+        esto es
+        un comentario 
+        con varias lineas
+        */
+    /**
+     * Esto identifica JavaDoc.
+     * pero también funciona como
+     * comentario en varias líneas.
+     */
+
+        byte byt3 = 1; //Byte números desde -128 hasta 127
+        short num1 = 500; //short números desde -32768 hasta 32767
+        int num2 = 69; //Entero
+        long num3 = 600; //Números grandes
+        float myFloat = 6.99f; //Número de punto flotante
+        double num4 = 6.88; //Números con decimales
+        char myLetra = 'v'; //Caracter
+        boolean myBool = true; //Boolean
+        boolean myBool2 = false;
+        //String myText = "Hello"; //String
+
+    public static void main(String[] args) {
+
+        String hello = "Hello, ";
+        final String lenguaje = "JAVA!";
+        
+        System.out.println(hello + lenguaje);
+    }
+}
